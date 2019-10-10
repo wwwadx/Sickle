299d1e80e5b4c20cfc8d34dadaa2492d9c783c08
wwwadx <wza19930703@gmail.com>
2019-10-10 16:45:03 +0800

    fix

--- sickle/factor/domians/normal_domian_1.py
+++ sickle/factor/domians/normal_domian_1.py
@@ -1,6 +1,6 @@
 domain_dict = {
     'energy_chemical': ['fu', 'bu', 'ru', 'l', 'v', 'pp', 'zc', 'ta', 'ma', 'fg'],
-    'ferrous': ['rb', 'hc', 'jm', 'j', 'i', 'sm', 'sf', 'zc'],
+    'ferrous': ['rb', 'hc', 'jm', 'j', 'sm', 'sf', 'zc'],
     'nonferrous': ['au', 'ag', 'pb', 'zn', 'al', 'cu', 'ni', 'sn'],
     'farm': ['a', 'ap', 'm', 'c', 'y', 'jd', 'p', 'cs', 'cf', 'sr', 'oi', 'rm']
 }
